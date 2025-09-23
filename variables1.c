95e422246430f060ba536eff11d53833d252765f
Brian28714-kesly <brian.28714@students.kyu.ac.ke>
2025-09-23 13:31:54 +0300

    Add files via upload

---
+++ variables1.c
@@ -0,0 +1,19 @@
+// Simple info program
+/*
+Name: Brian Cheruiyot
+Reg no: PA106/G/28714/25
+Description: Program to display my information
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Fundamentals of system analysis and design\n");
+    printf("Discrete mathematics\n");
+    printf("Mathematics for science\n");
+    printf("Introduction to programming and problem solving\n");
+    printf("Computer system and organization\n");
+    printf("Communication skills\n");
+    printf("Fundamentals of ICT\n");
+
+    return (0);} 
